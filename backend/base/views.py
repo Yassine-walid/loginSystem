99eaf62b4a09fe6ff0feec0a09774b4dba54b705
Yassine-walid <yassine1998walid@gmail.com>
2022-04-09 14:58:59 +0000

    Initial commit : Setup files

---
+++ backend/base/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request,"index.html")
+
+def signup(request):
+    return render(request,"signup.html")
+
+def signin(request):
+    return render(request,"signin.html")
+
+def signout(request):
+    pass
